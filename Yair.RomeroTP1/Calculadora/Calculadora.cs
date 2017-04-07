8e893c89893dcad1dcede3056b7d0e2cae04700e
romeroyahir <yahirmromero@gmail.com>
2017-04-07 16:10:16 -0300

    Yair Romero - TP1: Calculadora
    Primer TP: Calculadora

---
+++ Yair.RomeroTP1/Calculadora/Calculadora.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Calculadora
+    {
+        #region Atributos
+        /// <summary>
+        /// Atributos y la iniciación del "resultado".
+        /// </summary>
+        double resultado = 00;
+        int op;
+        #endregion
+
+        #region Metodos
+
+        /// <summary>
+        /// Realizará la operación que indique el operador entre los dos objetos Numero que reciba.
+        /// </summary>
+        /// <param name="numero1"></param>
+        /// <param name="numero2"></param>
+        /// <param name="operador"></param>
+        /// <returns>Resultado (Double)</returns>
+        public double Operar(Numero numero1, Numero numero2, string operador)
+        {
+            ValidarOperador(operador); 
+            op = int.Parse(operador);
+            switch (op)
+            {
+                case 0:
+                          resultado = numero1.GetNumero() + numero2.GetNumero();
+                          break;
+                case 1:
+                          resultado = numero1.GetNumero() - numero2.GetNumero();
+                          break;
+                case 2:
+                          resultado = numero1.GetNumero() * numero2.GetNumero();
+                          break;
+                case 3: 
+                          if (numero2.GetNumero() == 0)
+                          {
+                              resultado = 0;
+                              break;
+                          }
+                          else
+                          {
+                              resultado = numero1.GetNumero() / numero2.GetNumero();
+                              break;
+                          }
+                          
+                          
+            }
+                    return resultado;
+
+        }
+
+        /// <summary>
+        /// Validará que el operador recibido sea un carácter válido
+        /// </summary>
+        /// <param name="operador"></param>
+        /// <returns></returns>
+        public string ValidarOperador(string operador)
+        {
+            int op;
+            op = int.Parse(operador);
+            if (op == -1)
+            {
+                op = 0;
+                return op.ToString();
+            }
+            else
+            {
+                
+                return op.ToString();
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ Yair.RomeroTP1/Calculadora/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Calculadora;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            //Limpio el label de resultado
+            lblResultado.Text = "";
+
+            //Operar
+            double txt1;
+            double txt2;
+            if (double.TryParse(txtNumero1.Text, out txt1) == true && double.TryParse(txtNumero2.Text, out txt2) == true)
+            {
+                Numero num1 = new Numero(txt1);
+                Numero num2 = new Numero(txt2);
+                
+                int indexcmb = cmbOperacion.SelectedIndex;
+                Calculadora calc = new Calculadora();
+                string comb = calc.ValidarOperador(indexcmb.ToString());
+
+                double bla;
+                bla = calc.Operar(num1, num2, comb);
+                lblResultado.Text = (bla.ToString());
+            }
+            else
+            {
+                MessageBox.Show("ERROR! Ingresaste valor(es) invalido(s), vuelve a ingresarlos.");
+                btnLimpiar.PerformClick();
+            }  
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtNumero1.Clear();
+            txtNumero2.Clear();
+            lblResultado.Text = "";
+            cmbOperacion.Text = "Operación";
+        }
+        
+        
+    }
+}
